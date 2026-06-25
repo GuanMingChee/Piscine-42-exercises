716c1be34ac905fac59229c6f41156c814032bc3
GuanMing Chee <guanmingchee11@gmail.com>
2026-06-26 01:24:46 +0800

    fix norm error

--- c_02/ex11/ft_putstr_non_printable.c
+++ c_02/ex11/ft_putstr_non_printable.c
@@ -5,20 +5,18 @@ void	ft_putstr_non_printable(char *str);
 void	ft_putstr_non_printable(char *str)
 {
 	int	i;
-	char	*search;
 	int	h;
 
 	i = 0;
-	search = "0123456789abcdef";
 	while (str[i])
 	{
 		if (str[i] < 32 || str[i] > 126)
 		{
 			write(1, "\\", 1);
 			h = str[i] / 16;
-			write(1, &search[h], 1);
+			write(1, &"0123456789abcdef"[h], 1);
 			h = str[i] % 16;
-			write(1, &search[h], 1);
+			write(1, &"0123456789abcdef"[h], 1);
 		}
 		else
 			write(1, &str[i], 1);
